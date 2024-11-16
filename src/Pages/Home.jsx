de7c1350a7830508d16a02947d0c81bf1a025602
Gantavya Bansal <gantavyaoo@gmail.com>
2024-11-16 16:53:51 +0530

    Update Home.jsx
    added a temp header and footer for ui/ux and some performance tests

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,14 +1,20 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import '../style/Home.css';
+import Header from '../components/header';
+import Footer from '../components/footer';
 
 const Home = () => {
     const [count, setCount] = useState(0);
     return (
+        <>
+        <Header />
         <div className='home-main'>
             <h1>Home</h1>
             <Link to="/braj">Braj Darshan</Link>
         </div>
+        <Footer />
+        </>
     );
 }
 
