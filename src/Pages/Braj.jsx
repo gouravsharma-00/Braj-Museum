6694b6fbbecc4b3e50ab6b28701f1c2e82bfde51
Gantavya Bansal <gantavyaoo@gmail.com>
2024-11-19 15:43:55 +0530

    Update Braj.jsx

--- src/Pages/Braj.jsx
+++ src/Pages/Braj.jsx
@@ -1,9 +1,13 @@
 import OpenSeadragonViewer from "../components/openseaLib";
 import '../styles/Braj.css';
+import MainSection from '../components/Header';
+import Footer from '../components/Footer';
 const Braj = () => {
   return(
     <>
+    <MainSection/>
       <OpenSeadragonViewer />
+    <Footer/>
     </>
   );
 }
