a1adec09acf6ff1c5419d9e5a2559255a8d51725
Gantavya Bansal <gantavyaoo@gmail.com>
2024-11-30 00:02:09 +0530

    Update Home.jsx
    added about component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,12 +3,14 @@ import HighLight from '../components/HighLight';
 import TopDestinations from '../components/TopDestinations';
 import MainSection from '../components/Header';
 import Footer from '../components/Footer';
+import Contributors from '../components/contributors';
 
 const Home = () => {
     return (
         <>
             <MainSection />
             <TopDestinations />
+            <Contributors/>
             <HighLight />
             <Footer />
         </>
